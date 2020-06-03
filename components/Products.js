288183ed04bbe00712c2697d7ae720b331c15455
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-06-04 01:05:54 +0530

    fix: loaders

--- components/Products.js
+++ components/Products.js
@@ -2,106 +2,112 @@ import React from 'react';
 import { View, FlatList } from 'react-native';
 import { useSubscription } from '@apollo/react-hooks';
 import {
-  INVENTORY_PRODUCTS,
-  SIMPLE_RECIPE_PRODUCTS,
-  CUSTOMIZABLE_PRODUCTS,
-  COMBO_PRODUCTS,
+   INVENTORY_PRODUCTS,
+   SIMPLE_RECIPE_PRODUCTS,
+   CUSTOMIZABLE_PRODUCTS,
+   COMBO_PRODUCTS,
 } from '../graphql';
-import { Spinner, Text } from 'native-base';
+import { Text } from 'native-base';
+import { Spinner } from '@ui-kitten/components';
 import Card from './Card';
 import { width } from '../utils/Scalaing';
 
 const Products = ({ category }) => {
-  console.log(category, 'rendered');
-  const [products, setProducts] = React.useState([]);
+   console.log(category, 'rendered');
+   const [products, setProducts] = React.useState([]);
 
-  // Subscriptions
-  const { loading: inventoryProductsLoading } = useSubscription(
-    INVENTORY_PRODUCTS,
-    {
+   // Subscriptions
+   const { loading: inventoryProductsLoading } = useSubscription(
+      INVENTORY_PRODUCTS,
+      {
+         variables: {
+            ids: category.inventoryProducts,
+         },
+         onSubscriptionData: data => {
+            console.log(data.subscriptionData.data.inventoryProducts);
+            setProducts([
+               ...products,
+               ...data.subscriptionData.data.inventoryProducts,
+            ]);
+         },
+      }
+   );
+
+   const { loading: simpleRecipeProductsLoading } = useSubscription(
+      SIMPLE_RECIPE_PRODUCTS,
+      {
+         variables: {
+            ids: category.simpleRecipeProducts,
+         },
+         onSubscriptionData: data => {
+            console.log(data.subscriptionData.data.simpleRecipeProducts);
+            setProducts([
+               ...products,
+               ...data.subscriptionData.data.simpleRecipeProducts,
+            ]);
+         },
+      }
+   );
+
+   const { loading: customizableProductsLoading } = useSubscription(
+      CUSTOMIZABLE_PRODUCTS,
+      {
+         variables: {
+            ids: category.customizableProducts,
+         },
+         onSubscriptionData: data => {
+            console.log(data.subscriptionData.data.customizableProducts);
+            setProducts([
+               ...products,
+               ...data.subscriptionData.data.customizableProducts,
+            ]);
+         },
+      }
+   );
+
+   const { loading: comboProductsLoading } = useSubscription(COMBO_PRODUCTS, {
       variables: {
-        ids: category.inventoryProducts,
+         ids: category.comboProducts,
       },
-      onSubscriptionData: (data) => {
-        console.log(data.subscriptionData.data.inventoryProducts);
-        setProducts([
-          ...products,
-          ...data.subscriptionData.data.inventoryProducts,
-        ]);
+      onSubscriptionData: data => {
+         console.log(data.subscriptionData.data.comboProducts);
+         setProducts([
+            ...products,
+            ...data.subscriptionData.data.comboProducts,
+         ]);
       },
-    }
-  );
+   });
 
-  const { loading: simpleRecipeProductsLoading } = useSubscription(
-    SIMPLE_RECIPE_PRODUCTS,
-    {
-      variables: {
-        ids: category.simpleRecipeProducts,
-      },
-      onSubscriptionData: (data) => {
-        console.log(data.subscriptionData.data.simpleRecipeProducts);
-        setProducts([
-          ...products,
-          ...data.subscriptionData.data.simpleRecipeProducts,
-        ]);
-      },
-    }
-  );
+   //  if (
+   //     inventoryProductsLoading ||
+   //     simpleRecipeProductsLoading ||
+   //     customizableProductsLoading ||
+   //     comboProductsLoading
+   //  )
+   //     return (
+   //        <View
+   //           style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+   //        >
+   //           <Spinner />
+   //        </View>
+   //     );
 
-  const { loading: customizableProductsLoading } = useSubscription(
-    CUSTOMIZABLE_PRODUCTS,
-    {
-      variables: {
-        ids: category.customizableProducts,
-      },
-      onSubscriptionData: (data) => {
-        console.log(data.subscriptionData.data.customizableProducts);
-        setProducts([
-          ...products,
-          ...data.subscriptionData.data.customizableProducts,
-        ]);
-      },
-    }
-  );
-
-  const { loading: comboProductsLoading } = useSubscription(COMBO_PRODUCTS, {
-    variables: {
-      ids: category.comboProducts,
-    },
-    onSubscriptionData: (data) => {
-      console.log(data.subscriptionData.data.comboProducts);
-      setProducts([...products, ...data.subscriptionData.data.comboProducts]);
-    },
-  });
-
-  if (
-    inventoryProductsLoading ||
-    simpleRecipeProductsLoading ||
-    customizableProductsLoading ||
-    comboProductsLoading
-  )
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Spinner />
+   return (
+      <View>
+         {products.length ? (
+            <>
+               <FlatList
+                  numColumns={width > 1000 ? 3 : 1}
+                  data={products}
+                  keyExtractor={item => item.id}
+                  renderItem={({ item: product }) => <Card product={product} />}
+               />
+            </>
+         ) : (
+            <Text> Oops! </Text>
+         )}
       </View>
-    );
-
-  return (
-    <View>
-      {products.length ? (
-        <>
-          <FlatList
-            numColumns={width > 1000 ? 3 : 1}
-            data={products}
-            keyExtractor={(item) => item.id}
-            renderItem={({ item: product }) => <Card product={product} />}
-          />
-        </>
-      ) : (
-        <Text> Oops! </Text>
-      )}
-    </View>
-  );
+   );
 };
 
 export default Products;
