56741668fdb361c521a4740948edc772a0f58723
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-06-11 23:26:25 +0530

    chore: pseudo logic

--- components/Fulfillment.js
+++ components/Fulfillment.js
@@ -632,3 +632,28 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
   },
 });
+
+/*
+Case : PREORDER PICKUP (all the logics are in similiar fashion)
+assume current time to be 15:00 (3pm) and date to be 11 June
+
+1. get all recurences using subscriptions, so everytime they change any option, they see latest values
+2. check if master pickup is on/off, if off then show message
+3. check if I got any recurrences or not, if 0 then show message
+4. pass recurrences to -> generatePickUpSlots(recurrences)
+   - loop over each recurrence
+   a- so let's say our first recurrence is 'DAILY'
+   --- get all dates that satify this recurrence within next 7 days.
+   --- loop over all the dates
+   b--- so let's say first date is 11 June (today)
+   ---- now in step 4a we have time slots, loop over all the time slots
+   c----- let's say first time slot is: 14:00 - 18:00      (2pm - 6pm) 
+   ------ check if current time + lead time < timeslot end time(to), if no then skip
+   ------ if yes then check, if current time + lead time > timeslot start time(from)
+   ------ if no -> then 14:00 - 18:00 is a perfect slot for this date(4b)
+   ------ if yes -> then new timeslot start time will be = current time + lead time, i.e. 15:00 - 18:00
+   ------ at the end of each step(4c), push data into an array according to dates, so all the time slots for same day will fall under one date
+5. now we have data grouped according to our need, pass this data into -> generateMiniSlots(data, 15) [second params is size in minutes]
+  - it will again loop over all the dates, slice all the time slots
+  - make another array with dates and each date will have these 15 mins timestamps.
+*/
