7a68c4f16e2119cfacdba9dc6f841407192d6eea
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-06-17 00:19:59 +0530

    fix: order screen

--- screens/OrderHistory/index.js
+++ screens/OrderHistory/index.js
@@ -7,6 +7,7 @@ import { useCartContext } from '../../context/cart';
 import { ORDERS } from '../../graphql';
 import { styles } from './styles';
 import { useSubscription } from '@apollo/react-hooks';
+import { Header } from '../../components';
 
 export default ({ navigation }) => {
   const { customer } = useCartContext();
@@ -42,6 +43,7 @@ export default ({ navigation }) => {
   if (error) {
     return (
       <>
+        <Header title='Home' navigation={navigation} />
         <View style={styles.center}>
           <Text>Oops! We could not get orders. Check again later!</Text>
         </View>
@@ -50,171 +52,181 @@ export default ({ navigation }) => {
   }
 
   return (
-    <>
-      <Text style={styles.heading}>Orders</Text>
-      <ScrollView style={styles.container}>
-        {data?.orders
-          .filter((order) => order?.deliveryInfo)
-          .map((order) => (
-            <>
-              <TouchableOpacity
-                key={order?.id}
-                // onPress={() => navigation.navigate('DeliveryScreen')}
-                style={styles.card}
-              >
-                <View style={styles.head}>
-                  <Text style={styles.title}>Order ID: {order?.id}</Text>
-                  <Text
-                    style={[
-                      styles.status,
-                      {
-                        backgroundColor: whatColor(order.orderStatus),
-                      },
-                    ]}
-                  >
-                    {order.orderStatus.replace(/_/g, ' ')}
+    <View style={{ backgroundColor: '#fff', flex: 1 }}>
+      <Header title='Home' navigation={navigation} />
+      <View style={styles.outerContainer}>
+        <Text style={styles.heading}>Order History</Text>
+        <ScrollView style={styles.container}>
+          {data?.orders
+            .filter((order) => order?.deliveryInfo)
+            .map((order) => (
+              <>
+                <TouchableOpacity
+                  key={order?.id}
+                  // onPress={() => navigation.navigate('DeliveryScreen')}
+                  style={styles.card}
+                >
+                  <View style={styles.head}>
+                    <Text style={styles.title}>Order ID: {order?.id}</Text>
+                    <Text
+                      style={[
+                        styles.status,
+                        {
+                          backgroundColor: whatColor(order.orderStatus),
+                        },
+                      ]}
+                    >
+                      {order.orderStatus.replace(/_/g, ' ')}
+                    </Text>
+                  </View>
+                  <Text style={[styles.muted, styles.bold]}>
+                    Ordered on:{' '}
+                    <Text style={styles.lite}>
+                      {moment(order?.created_at).format('LLLL')}
+                    </Text>
                   </Text>
-                </View>
-                <Text style={[styles.muted, styles.bold]}>
-                  Ordered on:{' '}
-                  <Text style={styles.lite}>
-                    {moment(order?.created_at).format('LLLL')}
-                  </Text>
-                </Text>
-                {/* <Text style={[styles.muted, styles.bold]}>
+                  {/* <Text style={[styles.muted, styles.bold]}>
                            Deliver on: <Text style={styles.lite}>NA</Text>
                         </Text> */}
-                <Text style={styles.muted}>
-                  {order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
-                    ?.line1 +
-                    ', ' +
-                    order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
-                      ?.line2 +
-                    ', ' +
-                    order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
-                      ?.city +
-                    ', ' +
-                    order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
-                      ?.state +
-                    ', ' +
-                    order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
-                      ?.country}{' '}
-                  -{' '}
-                  {
-                    order?.deliveryInfo.dropoff?.dropoffInfo?.customerAddress
-                      ?.zipcode
-                  }
-                </Text>
-                <Accordion
-                  headerStyle={styles.header}
-                  dataArray={[
+                  <Text style={styles.muted}>
+                    {order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
+                      ?.line1 +
+                      ', ' +
+                      order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
+                        ?.line2 +
+                      ', ' +
+                      order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
+                        ?.city +
+                      ', ' +
+                      order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
+                        ?.state +
+                      ', ' +
+                      order?.deliveryInfo?.dropoff?.dropoffInfo?.customerAddress
+                        ?.country}{' '}
+                    -{' '}
                     {
-                      title: `${
-                        order.orderInventoryProducts.length +
-                        order.orderMealKitProducts.length +
-                        order.orderReadyToEatProducts.length
-                      }  Item(s)`,
-                      content: (
-                        <View style={{ width: '100%' }}>
-                          {order.orderInventoryProducts.map((product) => (
-                            <View style={styles.product}>
-                              <View style={styles.productInfo}>
-                                <Text>{product.inventoryProduct.name}</Text>
-                                <Text style={styles.productOption}>
-                                  {product.inventoryProductOption.label}
-                                </Text>
-                              </View>
-                              <Text style={styles.productPrice}>
-                                ${' '}
-                                {product.inventoryProductOption.price[0].value}
-                              </Text>
-                            </View>
-                          ))}
-                          {order.orderMealKitProducts.map((product) => (
-                            <View style={styles.product}>
-                              <View style={styles.productInfo}>
-                                <Text>{product.simpleRecipeProduct.name}</Text>
-                                <Text style={styles.productOption}>
-                                  Meal Kit Serving:{' '}
+                      order?.deliveryInfo.dropoff?.dropoffInfo?.customerAddress
+                        ?.zipcode
+                    }
+                  </Text>
+                  <Accordion
+                    headerStyle={styles.header}
+                    dataArray={[
+                      {
+                        title: `${
+                          order.orderInventoryProducts.length +
+                          order.orderMealKitProducts.length +
+                          order.orderReadyToEatProducts.length
+                        }  Item(s)`,
+                        content: (
+                          <View style={{ width: '100%' }}>
+                            {order.orderInventoryProducts.map((product) => (
+                              <View style={styles.product}>
+                                <View style={styles.productInfo}>
+                                  <Text>{product.inventoryProduct.name}</Text>
+                                  <Text style={styles.productOption}>
+                                    {product.inventoryProductOption.label}
+                                  </Text>
+                                </View>
+                                <Text style={styles.productPrice}>
+                                  ${' '}
                                   {
-                                    product.simpleRecipeProductOption
-                                      .simpleRecipeYield.yield.serving
+                                    product.inventoryProductOption.price[0]
+                                      .value
                                   }
                                 </Text>
                               </View>
-                              <Text style={styles.productPrice}>
-                                ${' '}
-                                {
-                                  product.simpleRecipeProductOption.price[0]
-                                    .value
-                                }
-                              </Text>
-                            </View>
-                          ))}
-                          {order.orderReadyToEatProducts.map((product) => (
-                            <View style={styles.product}>
-                              <View style={styles.productInfo}>
-                                <Text>{product.simpleRecipeProduct.name}</Text>
-                                <Text style={styles.productOption}>
-                                  Ready to Eat Serving:{' '}
+                            ))}
+                            {order.orderMealKitProducts.map((product) => (
+                              <View style={styles.product}>
+                                <View style={styles.productInfo}>
+                                  <Text>
+                                    {product.simpleRecipeProduct.name}
+                                  </Text>
+                                  <Text style={styles.productOption}>
+                                    Meal Kit Serving:{' '}
+                                    {
+                                      product.simpleRecipeProductOption
+                                        .simpleRecipeYield.yield.serving
+                                    }
+                                  </Text>
+                                </View>
+                                <Text style={styles.productPrice}>
+                                  ${' '}
                                   {
-                                    product.simpleRecipeProductOption
-                                      .simpleRecipeYield.yield.serving
+                                    product.simpleRecipeProductOption.price[0]
+                                      .value
                                   }
                                 </Text>
                               </View>
-                              <Text style={styles.productPrice}>
-                                ${' '}
-                                {
-                                  product.simpleRecipeProductOption.price[0]
-                                    .value
-                                }
-                              </Text>
-                            </View>
-                          ))}
-                        </View>
-                      ),
-                    },
-                    {
-                      title: 'Bill Details',
-                      content: (
-                        <View style={styles.rowContainer}>
-                          <View style={styles.row}>
-                            <Text>Txn ID</Text>
-                            <Text>{order.transactionId}</Text>
+                            ))}
+                            {order.orderReadyToEatProducts.map((product) => (
+                              <View style={styles.product}>
+                                <View style={styles.productInfo}>
+                                  <Text>
+                                    {product.simpleRecipeProduct.name}
+                                  </Text>
+                                  <Text style={styles.productOption}>
+                                    Ready to Eat Serving:{' '}
+                                    {
+                                      product.simpleRecipeProductOption
+                                        .simpleRecipeYield.yield.serving
+                                    }
+                                  </Text>
+                                </View>
+                                <Text style={styles.productPrice}>
+                                  ${' '}
+                                  {
+                                    product.simpleRecipeProductOption.price[0]
+                                      .value
+                                  }
+                                </Text>
+                              </View>
+                            ))}
                           </View>
-                          <View style={styles.row}>
-                            <Text>Delivery Price</Text>
-                            <Text>$ {order.deliveryPrice}</Text>
-                          </View>
-                          {order.tip && (
+                        ),
+                      },
+                      {
+                        title: 'Bill Details',
+                        content: (
+                          <View style={styles.rowContainer}>
                             <View style={styles.row}>
-                              <Text>Tip</Text>
-                              <Text>$ {order.tip}</Text>
+                              <Text>Txn ID</Text>
+                              <Text>{order.transactionId}</Text>
                             </View>
-                          )}
-                          <View style={styles.row}>
-                            <Text>Tax</Text>
-                            <Text>$ {order.tax}</Text>
+                            <View style={styles.row}>
+                              <Text>Delivery Price</Text>
+                              <Text>$ {order.deliveryPrice}</Text>
+                            </View>
+                            {order.tip && (
+                              <View style={styles.row}>
+                                <Text>Tip</Text>
+                                <Text>$ {order.tip}</Text>
+                              </View>
+                            )}
+                            <View style={styles.row}>
+                              <Text>Tax</Text>
+                              <Text>$ {order.tax}</Text>
+                            </View>
+                            <View style={styles.row}>
+                              <Text>Total Price</Text>
+                              <Text>$ {order.itemTotal}</Text>
+                            </View>
                           </View>
-                          <View style={styles.row}>
-                            <Text>Total Price</Text>
-                            <Text>$ {order.itemTotal}</Text>
-                          </View>
-                        </View>
-                      ),
-                    },
-                  ]}
-                />
-                <View style={styles.flexContainer}>
-                  <Text style={styles.total}>Total</Text>
-                  <Text style={styles.total}>$ {order?.itemTotal}</Text>
-                </View>
-              </TouchableOpacity>
-              <Divider />
-            </>
-          ))}
-      </ScrollView>
-    </>
+                        ),
+                      },
+                    ]}
+                  />
+                  <View style={styles.flexContainer}>
+                    <Text style={styles.total}>Total</Text>
+                    <Text style={styles.total}>$ {order?.itemTotal}</Text>
+                  </View>
+                </TouchableOpacity>
+                <Divider />
+              </>
+            ))}
+        </ScrollView>
+      </View>
+    </View>
   );
 };
