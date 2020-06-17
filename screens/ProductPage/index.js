c159c2bd0a797d317e30a0a54941c096d2e37b06
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-06-18 02:11:35 +0530

    feat: add product description

--- screens/ProductPage/index.js
+++ screens/ProductPage/index.js
@@ -110,15 +110,7 @@ const ProductPage = ({ navigation, route }) => {
                 ))}
               </View>
               <Text style={styles.sectionTitle}>Description</Text>
-              <Text style={styles.text}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum
-              </Text>
+              <Text style={styles.text}>{product?.description}</Text>
               {product?.__typename?.includes('simpleRecipeProduct') && (
                 <View style={{ marginBottom: 10 }}>
                   <View style={{ flexDirection: 'row', width: 250 }}>
