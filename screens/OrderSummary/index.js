ff55193b9288c2116e92072d0b3446ce74f4d7f3
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-06-19 02:16:53 +0530

    feat: empty cart screen

--- screens/OrderSummary/index.js
+++ screens/OrderSummary/index.js
@@ -1,4 +1,4 @@
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, Feather } from '@expo/vector-icons';
 import React, { lazy } from 'react';
 import {
   SafeAreaView,
@@ -21,73 +21,99 @@ import { height } from '../../utils/Scalaing';
 import { styles } from './styles';
 import Header from '../../components/Header';
 import { useDrawerContext } from '../../context/drawer';
+import { useAppContext } from '../../context/app';
 
 const OrderSummary = ({ navigation, ...restProps }) => {
   const { cart } = useCartContext();
   const { open } = useDrawerContext();
+  const { visual } = useAppContext();
 
   let cartItems = cart?.cartInfo?.products;
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
       <Header title='Home' navigation={navigation} />
       {/* <HeaderBack title='Go Back' navigation={navigation} /> */}
-      <ScrollView>
-        <View style={styles.wrapper}>
-          <View style={styles.summary_title_conatiner}>
-            <View style={styles.summary_title_conatiner_left}>
-              <Text style={styles.summary_title_text}>Order Summary</Text>
+      {cartItems?.length ? (
+        <ScrollView>
+          <View style={styles.wrapper}>
+            <View style={styles.summary_title_conatiner}>
+              <View style={styles.summary_title_conatiner_left}>
+                <Text style={styles.summary_title_text}>Order Summary</Text>
+              </View>
+              <View style={styles.summary_title_conatiner_right}>
+                <Text
+                  style={[styles.summary_title_text, { textAlign: 'right' }]}
+                >
+                  {cartItems?.length} products
+                </Text>
+              </View>
             </View>
-            <View style={styles.summary_title_conatiner_right}>
-              <Text style={[styles.summary_title_text, { textAlign: 'right' }]}>
-                {cartItems?.length} products
-              </Text>
+            <View style={styles.title_container}>
+              {cart?.fulfillmentInfo ? (
+                <>
+                  <View style={styles.title_container_left}>
+                    <Text style={styles.deliver_on_text}>
+                      {cart?.fulfillmentInfo?.type?.replace('_', ' ')}
+                    </Text>
+                    <Text style={styles.time_text}>
+                      {cart?.fulfillmentInfo?.date}
+                    </Text>
+                  </View>
+                  <View style={styles.title_container_middle}>
+                    <Text
+                      style={[
+                        styles.time_text,
+                        { textAlign: 'center', flex: 1 },
+                      ]}
+                    >
+                      {cart?.fulfillmentInfo?.slot?.time}
+                    </Text>
+                  </View>
+                </>
+              ) : (
+                <Text>Oops! We couldn't set default preference for you. </Text>
+              )}
+              <View style={styles.title_container_right}>
+                <TouchableOpacity
+                  onPress={() => open('Fulfillment')}
+                  style={styles.edit}
+                >
+                  <Text style={styles.edit_text}>edit{'  '}</Text>
+                  <Ionicons
+                    style={{ paddingTop: 2 }}
+                    size={16}
+                    name='ios-arrow-forward'
+                  />
+                </TouchableOpacity>
+              </View>
             </View>
+            {cartItems?.map((item, index) => {
+              return <Summary item={item} key={index} />;
+            })}
+            <BillingDetails />
+            <DefaultPaymentFloater navigation={navigation} />
+            <DefaultAddressFloater navigation={navigation} />
+            <View style={{ height: height * 0.08 }} />
           </View>
-          <View style={styles.title_container}>
-            {cart?.fulfillmentInfo ? (
-              <>
-                <View style={styles.title_container_left}>
-                  <Text style={styles.deliver_on_text}>
-                    {cart?.fulfillmentInfo?.type?.replace('_', ' ')}
-                  </Text>
-                  <Text style={styles.time_text}>
-                    {cart?.fulfillmentInfo?.date}
-                  </Text>
-                </View>
-                <View style={styles.title_container_middle}>
-                  <Text
-                    style={[styles.time_text, { textAlign: 'center', flex: 1 }]}
-                  >
-                    {cart?.fulfillmentInfo?.slot?.time}
-                  </Text>
-                </View>
-              </>
-            ) : (
-              <Text>Oops! We couldn't set default preference for you. </Text>
-            )}
-            <View style={styles.title_container_right}>
-              <TouchableOpacity
-                onPress={() => open('Fulfillment')}
-                style={styles.edit}
-              >
-                <Text style={styles.edit_text}>edit{'  '}</Text>
-                <Ionicons
-                  style={{ paddingTop: 2 }}
-                  size={16}
-                  name='ios-arrow-forward'
-                />
-              </TouchableOpacity>
-            </View>
-          </View>
-          {cartItems?.map((item, index) => {
-            return <Summary item={item} key={index} />;
-          })}
-          <BillingDetails />
-          <DefaultPaymentFloater navigation={navigation} />
-          <DefaultAddressFloater navigation={navigation} />
-          <View style={{ height: height * 0.08 }} />
+        </ScrollView>
+      ) : (
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <Text
+            style={{
+              marginBottom: 10,
+              fontWeight: 'bold',
+              fontSize: 24,
+              color: '#666',
+            }}
+          >
+            Awww... This feels so light. Oh wait! It's empty.
+          </Text>
+          <Feather name='shopping-cart' size={64} color={visual.color} />
         </View>
-      </ScrollView>
+      )}
+
       <CartSummary
         {...restProps}
         navigation={navigation}
