c66372c648971b4676eaf2fa1e7ed1b03cb55818
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-06-26 20:28:00 +0530

    fix: recipe card styling

--- components/SimpleProductItem/styles.js
+++ components/SimpleProductItem/styles.js
@@ -9,30 +9,15 @@ export const styles = EStyleSheet.create({
     paddingBottom: 20,
     paddingHorizontal: 10,
     marginTop: 4,
-    borderBottomWidth: 1,
+    borderBottomWidth: 0,
     backgroundColor: '#fff',
     borderBottomColor: '#fff',
   },
   item_container_one: {
     flex: 4,
+    position: 'relative',
     paddingTop: 20,
     height: height * 0.3,
-  },
-  item_container_three: {
-    flex: 2,
-    paddingTop: 15,
-  },
-  item_image: {
-    flex: 1,
-    height: null,
-    width: null,
-    resizeMode: 'contain',
-  },
-  item_chef: {
-    fontSize: 18,
-    color: '#4f4e4e',
-    flex: 1,
-    textAlign: 'right',
   },
   item_container_two: {
     flex: 1.3,
@@ -42,19 +27,30 @@ export const styles = EStyleSheet.create({
     paddingTop: 15,
     paddingLeft: 10,
   },
-  item_title: {
-    fontSize: '$l',
+  item_container_three: {
+    flex: 1.3,
+    paddingTop: 15,
   },
-  item_three_lower: {
+  bottom_container: {
     flex: 1,
-    alignItems: 'center',
+    justifyContent: 'center',
     flexDirection: 'row',
+    paddingHorizontal: 20,
   },
-  item_details: {
-    textAlign: 'left',
+  item_image_title: {
+    position: 'absolute',
+    zIndex: 8,
+    color: 'gray',
     fontWeight: 'bold',
-    fontSize: '$s',
+  },
+  item_image: {
     flex: 1,
+    height: null,
+    width: null,
+    resizeMode: 'contain',
+  },
+  item_title: {
+    fontSize: '$m',
   },
   item_chef: {
     fontSize: 18,
@@ -62,23 +58,93 @@ export const styles = EStyleSheet.create({
     flex: 1,
     textAlign: 'right',
   },
-  type_container_right: {
-    flexDirection: 'row',
+  something: {
+    fontSize: '$s',
+    color: '#4f4e4e',
+    marginVertical: 10,
   },
-  type_button: {
-    backgroundColor: '#fff',
-    padding: 10,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+  item_category: {
+    color: 'white',
+    width: 70,
+    textAlign: 'center',
+    marginTop: 5,
+    paddingVertical: 2,
+    borderRadius: 2,
+    fontSize: '$s',
+  },
+  options_text: {
+    color: '#3fa4fd',
+    textAlign: 'right',
+    fontSize: '$s',
+  },
+  item_details: {
+    textAlign: 'left',
+    fontWeight: 'bold',
+    fontSize: '$s',
+    flex: 1,
+  },
+  price: {
+    flex: 1,
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#efefef',
+    flexDirection: 'row',
   },
-  type_text: {
+  add_to_cart_container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  button: {
+    backgroundColor: '#3fa4ff',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+  },
+  add_to_card_text: {
+    color: 'white',
+    fontSize: '$s',
+  },
+  price_text: {
+    fontSize: '$l',
     fontWeight: 'bold',
   },
+  details: {
+    justifyContent: 'center',
+  },
+  type_container: {
+    height: height * 0.1,
+    flexDirection: 'row',
+  },
+  type_container_right: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingVertical: 15,
+  },
+  type_button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: '#f1f1f1',
+    paddingHorizontal: 10,
+  },
   done_container: {
-    marginLeft: 8,
-    borderRadius: '50%',
+    backgroundColor: '#3fa4ff',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 5,
+    height: 20,
+    width: 20,
+  },
+  item_three_upper: {
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    flex: 1,
+  },
+  item_three_lower: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
