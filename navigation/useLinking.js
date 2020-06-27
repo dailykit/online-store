10bd873a78c6feb6e2c008d73626e0cd12765886
Praveen Bisht <prvnbist@gmail.com>
2020-06-27 20:31:06 +0530

    fix: initialState on reload

--- navigation/useLinking.js
+++ navigation/useLinking.js
@@ -1,42 +1,33 @@
-import { useLinking } from '@react-navigation/native';
-import { Linking } from 'expo';
+import { useLinking } from "@react-navigation/native";
+import { Linking } from "expo";
 
 export default function (containerRef) {
   return useLinking(containerRef, {
-    prefixes: [Linking.makeUrl('/')],
+    prefixes: [Linking.makeUrl("/")],
     config: {
       App: {
-        path: '/store',
+        path: "/store",
         screens: {
           Home: {
-            path: '/',
-            screens: {
-              Home: {
-                path: 'home/',
-              },
-              ProductPage: {
-                path: 'product',
-                parse: {
-                  id: Number,
-                  type: String,
-                },
-                stringify: {
-                  data: (data) => '',
-                },
-              },
-              CategoryProductsPage: {
-                path: 'CategoryProductsPage',
-                parse: {
-                  data: (data) => JSON.parse(decodeURIComponent(data)),
-                  category: (category) =>
-                    JSON.parse(decodeURIComponent(category)),
-                },
-                stringify: {
-                  data: (data) => encodeURIComponent(JSON.stringify(data)),
-                  category: (category) =>
-                    encodeURIComponent(JSON.stringify(category)),
-                },
-              },
+            path: "/",
+          },
+          ProductPage: {
+            path: "product",
+            parse: {
+              id: Number,
+              type: String,
+            },
+          },
+          CategoryProductsPage: {
+            path: "CategoryProductsPage",
+            parse: {
+              data: (data) => JSON.parse(decodeURIComponent(data)),
+              category: (category) => JSON.parse(decodeURIComponent(category)),
+            },
+            stringify: {
+              data: (data) => encodeURIComponent(JSON.stringify(data)),
+              category: (category) =>
+                encodeURIComponent(JSON.stringify(category)),
             },
           },
         },
