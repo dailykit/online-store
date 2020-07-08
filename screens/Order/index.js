63c59e85b4617dcd60cac81739d4d80adf7f4a83
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-07-08 15:03:16 +0530

    feat: delivery order status

--- screens/Order/index.js
+++ screens/Order/index.js
@@ -1,21 +1,17 @@
 import { AntDesign, Feather } from '@expo/vector-icons'
-import React, { lazy } from 'react'
-import {
-   Image,
-   SafeAreaView,
-   ScrollView,
-   Text,
-   TouchableOpacity,
-   View,
-} from 'react-native'
+import React from 'react'
+import { SafeAreaView, ScrollView, Text, View, Image } from 'react-native'
 import { useCartContext } from '../../context/cart'
 import { styles } from './styles'
 import { useAppContext } from '../../context/app'
 import { useSubscription } from '@apollo/react-hooks'
 import { ORDER } from '../../graphql'
 import { Header } from '../../components'
-import { Spinner, Accordion } from 'native-base'
+import { Spinner } from 'native-base'
 import OrderCard from '../../components/OrderCard'
+import orderdelivered from '../../assets/imgs/orderdelivered.png'
+import orderpickup from '../../assets/imgs/orderpickup.png'
+import orderpreparing from '../../assets/imgs/orderpreparing.png'
 
 const Order = ({ route, navigation }) => {
    const { orderId } = route.params
@@ -34,7 +30,7 @@ const Order = ({ route, navigation }) => {
    )
 
    return (
-      <SafeAreaView style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
          <Header title={brand.name} navigation={navigation} />
          {loading ? (
             <View
@@ -47,24 +43,45 @@ const Order = ({ route, navigation }) => {
                <Spinner />
             </View>
          ) : (
-            <ScrollView style={styles.conatiner}>
-               <View style={styles.image_container}>
-                  <Feather name="check-circle" size={48} color={visual.color} />
-               </View>
-               <Text
-                  style={[styles.order_placed_title, { color: visual.color }]}
-               >
-                  Order Placed!
-               </Text>
-               {/* Conditional rendering */}
-               {order.fulfillmentType.includes('DELIVERY') ? (
-                  <Delivery />
-               ) : (
-                  <Pickup />
-               )}
-               {/* Order Details */}
-               <View>
-                  <OrderCard order={order} />
+            <ScrollView style={{ flex: 1 }}>
+               <View style={styles.container}>
+                  {/* <View style={{ marginBottom: 20 }}>
+                     <View style={styles.image_container}>
+                        <Feather
+                           name="check-circle"
+                           size={48}
+                           color={visual.color}
+                        />
+                     </View>
+                     <Text
+                        style={[
+                           styles.order_placed_title,
+                           { color: visual.color },
+                        ]}
+                     >
+                        Order Placed!
+                     </Text>
+                  </View> */}
+                  {/* Conditional rendering */}
+                  <View
+                     style={{
+                        marginBottom: 20,
+                     }}
+                  >
+                     {order.fulfillmentType.includes('DELIVERY') ? (
+                        <Delivery order={order} />
+                     ) : (
+                        <Pickup />
+                     )}
+                  </View>
+                  {/* Order Details */}
+                  <View
+                     style={{
+                        marginBottom: 20,
+                     }}
+                  >
+                     <OrderCard order={order} />
+                  </View>
                </View>
             </ScrollView>
          )}
@@ -74,10 +91,97 @@ const Order = ({ route, navigation }) => {
 
 export default Order
 
-const Delivery = () => {
+const Delivery = ({ order }) => {
+   const renderOrderStatus = status => {
+      switch (status) {
+         case 'PENDING':
+            return 'Order confirmed!'
+         case 'UNDER_PROCESSING':
+            return 'Your order is being prepared!'
+         case 'READY_TO_DISPATCH':
+            return 'Your order is ready! Waiting for delivery partner...'
+         case 'OUT_FOR_DELIVERY':
+            return 'Your order is out for delivery!'
+         case 'DELIVERED':
+            return 'Your order has been delivered!'
+      }
+   }
+
    return (
       <View>
-         <Text>Delivery</Text>
+         {/* Map */}
+         {order.deliveryInfo.tracking.location.isAvailable && <View></View>}
+         {/* Illustration */}
+         <View style={styles.orderStatus}>
+            <View style={styles.orderStatusIllustration}>
+               <View
+                  style={[
+                     styles.orderStatusImageContainer,
+                     {
+                        backgroundColor: [
+                           'PENDING',
+                           'UNDER_PROCESSING',
+                           'READY_TO_DISPATCH',
+                           'OUT_FOR_DELIVERY',
+                           'DELIVERED',
+                        ].includes(order.orderStatus)
+                           ? '#ddefff'
+                           : '#fff',
+                     },
+                  ]}
+               >
+                  <Image
+                     source={orderpreparing}
+                     style={{ height: '80%', width: '80%' }}
+                  />
+               </View>
+               <View
+                  style={[
+                     styles.orderStatusImageContainer,
+                     {
+                        backgroundColor: [
+                           'OUT_FOR_DELIVERY',
+                           'DELIVERED',
+                        ].includes(order.orderStatus)
+                           ? '#ddefff'
+                           : '#fff',
+                     },
+                  ]}
+               >
+                  <Image
+                     source={orderpickup}
+                     style={{ height: '80%', width: '80%' }}
+                  />
+               </View>
+               <View
+                  style={[
+                     styles.orderStatusImageContainer,
+                     {
+                        backgroundColor: ['DELIVERED'].includes(
+                           order.orderStatus
+                        )
+                           ? '#ddefff'
+                           : '#fff',
+                     },
+                  ]}
+               >
+                  <Image
+                     source={orderdelivered}
+                     style={{ height: '80%', width: '80%' }}
+                  />
+               </View>
+            </View>
+            <Text style={styles.orderStatusText}>
+               {renderOrderStatus(order.orderStatus)}
+            </Text>
+         </View>
+         {/* Tracking Code */}
+         {order.deliveryInfo.tracking.code.isAvailable && (
+            <Text>
+               You can also track your order here:
+               {order.deliveryInfo.tracking.code.url}
+            </Text>
+         )}
       </View>
    )
 }
