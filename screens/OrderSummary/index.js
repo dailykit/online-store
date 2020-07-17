7468d18eb1699ccccdeff0516283dfe6049bb21e
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-07-17 18:19:15 +0530

    fix: cart design

--- screens/OrderSummary/index.js
+++ screens/OrderSummary/index.js
@@ -21,6 +21,7 @@ import { useDrawerContext } from '../../context/drawer'
 import { useAppContext } from '../../context/app'
 import Fulfillment from '../../components/Fulfillment'
 import { useAuth } from '../../context/auth'
+import styled from 'styled-components/native'
 
 const OrderSummary = ({ navigation, ...restProps }) => {
    const { cart } = useCartContext()
@@ -34,142 +35,31 @@ const OrderSummary = ({ navigation, ...restProps }) => {
    return (
       <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
          <Header title="Home" navigation={navigation} />
-         {cartItems?.length ? (
-            <>
-               <View style={styles.topBar}>
-                  <Text style={styles.heading}>Cart</Text>
-                  <Text style={styles.count}>{`${cartItems?.length} Item${
-                     cartItems?.length > 1 ? 's' : ''
-                  }`}</Text>
-               </View>
-               <ScrollView style={styles.scrollView}>
-                  <View style={{ marginBottom: 20 }}>
-                     {cartItems?.map((item, index) => {
-                        return <Summary item={item} key={index} />
-                     })}
-                  </View>
-                  <BillingDetails />
-                  {isAuthenticated ? (
-                     <>
-                        <Text
-                           style={{
-                              fontWeight: 'bold',
-                              color: '#666',
-                              marginBottom: 5,
-                           }}
-                        >
-                           Fulfillment:
-                        </Text>
-                        {!editing ? (
-                           <View style={styles.title_container}>
-                              {cart?.fulfillmentInfo ? (
-                                 <>
-                                    <View style={styles.title_container_left}>
-                                       <Text
-                                          style={[
-                                             styles.deliver_on_text,
-                                             { color: visual.color },
-                                          ]}
-                                       >
-                                          {cart?.fulfillmentInfo?.type?.replace(
-                                             '_',
-                                             ' '
-                                          )}
-                                       </Text>
-                                       <Text style={styles.time_text}>
-                                          {moment
-                                             .parseZone(
-                                                cart?.fulfillmentInfo?.slot
-                                                   ?.from
-                                             )
-                                             .format('MMMM Do YYYY, h:mm a')}
-                                       </Text>
-                                    </View>
-                                 </>
-                              ) : (
-                                 <Text>
-                                    Oops! We couldn't set default preference for
-                                    you.
-                                 </Text>
-                              )}
-                              <View style={styles.title_container_right}>
-                                 <TouchableOpacity
-                                    onPress={() => setEditing(true)}
-                                    style={styles.edit}
-                                 >
-                                    <Feather
-                                       name="edit"
-                                       size={16}
-                                       color="#333"
-                                    />
-                                 </TouchableOpacity>
-                              </View>
-                           </View>
-                        ) : (
-                           <View style={{ marginBottom: 10 }}>
-                              <Fulfillment
-                                 navigation={navigation}
-                                 setEditing={setEditing}
-                              />
-                           </View>
-                        )}
-                        {cart.fulfillmentInfo?.type?.includes('DELIVERY') && (
-                           <View style={{ marginBottom: 10 }}>
-                              <Text
-                                 style={{
-                                    fontWeight: 'bold',
-                                    color: '#666',
-                                    marginBottom: 5,
-                                 }}
-                              >
-                                 Address selected for deilvery:
-                              </Text>
-                              <Text>{`${cart.address.line1}, ${cart.address.line2}, ${cart.address.city}, ${cart.address.state}, ${cart.address.country}`}</Text>
-                           </View>
-                        )}
-                        <Text
-                           style={{
-                              fontWeight: 'bold',
-                              color: '#666',
-                              marginBottom: 5,
-                           }}
-                        >
-                           Payment card:
-                        </Text>
-                        <View style={{ marginBottom: 20 }}>
-                           <DefaultPaymentFloater navigation={navigation} />
-                        </View>
-                        <View style={{ height: height * 0.1 }} />
-                     </>
-                  ) : (
-                     <View style={{ textAlign: 'center' }}>
-                        <Text
-                           style={{
-                              fontSize: '1.1rem',
-                              fontWeight: 'bold',
-                           }}
-                        >
-                           Almost there!
-                        </Text>
-                        <Text style={{ color: '#666', marginBottom: 10 }}>
-                           Login or Singup to place your order
-                        </Text>
-                        <TouchableOpacity
-                           onPress={() => open('Login')}
-                           style={{
-                              width: '100%',
-                              backgroundColor: visual.color,
-                              padding: 10,
-                           }}
-                        >
-                           <Text style={{ fontWeight: 'bold', color: '#fff' }}>
-                              CONTINUE
-                           </Text>
-                        </TouchableOpacity>
-                     </View>
-                  )}
-               </ScrollView>
-            </>
+         {!cartItems?.length ? (
+            <GridLayout>
+               <Checkout>
+                  <CheckoutSection>
+                     <CheckoutSectionHeading>Account</CheckoutSectionHeading>
+                     <CheckoutSectionContent>
+                        <CheckoutSectionText>
+                           To place your order now, log in to your existing
+                           account or sign up.
+                        </CheckoutSectionText>
+                     </CheckoutSectionContent>
+                  </CheckoutSection>
+                  <CheckoutSection>
+                     <CheckoutSectionHeading disabled>
+                        Fulfillment
+                     </CheckoutSectionHeading>
+                  </CheckoutSection>
+                  <CheckoutSection>
+                     <CheckoutSectionHeading disabled>
+                        Payment
+                     </CheckoutSectionHeading>
+                  </CheckoutSection>
+               </Checkout>
+               <Cart />
+            </GridLayout>
          ) : (
             <View
                style={{
@@ -206,17 +96,244 @@ const OrderSummary = ({ navigation, ...restProps }) => {
                </TouchableOpacity>
             </View>
          )}
-         {isAuthenticated && (
-            <CartSummary
-               {...restProps}
-               navigation={navigation}
-               text="CONFIRM AND PAY"
-               to="PaymentProcessing"
-               pay
-            />
-         )}
       </SafeAreaView>
    )
 }
 
 export default OrderSummary
+
+const Cart = () => {
+   return (
+      <StyledCart>
+         <CartHeader>
+            <CartHeaderTextLeft>Total Items</CartHeaderTextLeft>
+            <CartHeaderTextRight>2</CartHeaderTextRight>
+         </CartHeader>
+         <CartItems>
+            <CartItem>
+               <CartItemLeft>
+                  <CartItemImage
+                     source={{ uri: 'https://via.placeholder.com/50' }}
+                  />
+                  <CartItemInfo>
+                     <CartItemName>Corona Beer</CartItemName>
+                     <CartItemLabel>1 bottle - 300ml</CartItemLabel>
+                  </CartItemInfo>
+               </CartItemLeft>
+               <CartItemRight>
+                  <CartItemQuantity></CartItemQuantity>
+                  <CartItemPrice>$ 1.99</CartItemPrice>
+               </CartItemRight>
+            </CartItem>
+            <CartItem>
+               <CartItemLeft>
+                  <CartItemImage
+                     source={{ uri: 'https://via.placeholder.com/50' }}
+                  />
+                  <CartItemInfo>
+                     <CartItemName>Chicken Biryani</CartItemName>
+                  </CartItemInfo>
+               </CartItemLeft>
+               <CartItemRight>
+                  <CartItemQuantity></CartItemQuantity>
+                  <CartItemPrice>$ 5</CartItemPrice>
+               </CartItemRight>
+            </CartItem>
+            <CartItem>
+               <CartItemLeft>
+                  <CartItemImage
+                     source={{ uri: 'https://via.placeholder.com/50' }}
+                  />
+                  <CartItemInfo>
+                     <CartItemName>Veg Biryani</CartItemName>
+                  </CartItemInfo>
+               </CartItemLeft>
+               <CartItemRight>
+                  <CartItemQuantity></CartItemQuantity>
+                  <CartItemPrice>$ 2</CartItemPrice>
+               </CartItemRight>
+            </CartItem>
+         </CartItems>
+         <CartBilling>
+            <CartBillingHeading>Bill Details</CartBillingHeading>
+            <CartBillingDetail>
+               <CartBillingDetailText>Item Total</CartBillingDetailText>
+               <CartBillingDetailText>$ 9.99</CartBillingDetailText>
+            </CartBillingDetail>
+            <CartBillingDetail>
+               <CartBillingDetailText>Delivery Fee</CartBillingDetailText>
+               <CartBillingDetailText>$ 1</CartBillingDetailText>
+            </CartBillingDetail>
+            <Divider margin="8px" />
+            <CartBillingDetail>
+               <CartBillingDetailText>Taxes and Charges</CartBillingDetailText>
+               <CartBillingDetailText>$ 1.09</CartBillingDetailText>
+            </CartBillingDetail>
+         </CartBilling>
+         <Divider color="#282c3f" height="2px" />
+         <CartFooter>
+            <CartFooterText>TO PAY</CartFooterText>
+            <CartFooterText>$15.99</CartFooterText>
+         </CartFooter>
+      </StyledCart>
+   )
+}
+
+const GridLayout = styled.View`
+   padding: 2rem;
+   display: grid;
+   background: #e9ecee;
+   grid-template-columns: 1fr 400px;
+   height: calc(100% - 66px);
+`
+
+const Checkout = styled.View`
+   margin-right: 32px;
+`
+
+const CheckoutSection = styled.View`
+   position: relative;
+   margin-left: 25px;
+   background: #fff;
+   margin-bottom: 20px;
+   padding: 35px 40px 39px;
+   border-radius: 4px;
+`
+
+const CheckoutSectionHeading = styled.Text`
+   font-size: 32px;
+   font-weight: 600;
+   color: ${props => (props.disabled ? '#93959f' : '#282C3F')};
+`
+
+const CheckoutSectionContent = styled.View`
+   padding: 16px 0px;
+`
+
+const CheckoutSectionText = styled.Text`
+   color: #7e808c;
+   font-weight: 400;
+   font-size: 16px;
+   line-height: 1.12;
+`
+
+const StyledCart = styled.View`
+   border-radius: 4px;
+   overflow: hidden;
+`
+
+const CartHeader = styled.View`
+   background: #fff;
+   padding: 10px 30px;
+   display: flex;
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+   shadow-opacity: 0.75;
+   shadow-radius: 5px;
+   shadow-color: #ccc;
+   shadow-offset: 5px 0px;
+`
+const CartHeaderTextLeft = styled.Text`
+   font-weight: bold;
+   color: #93808c;
+`
+
+const CartHeaderTextRight = styled.Text`
+   font-weight: bold;
+   color: #282c3f;
+`
+
+const CartItems = styled.View`
+   max-height: 300px;
+   overflow-y: scroll;
+   background: #fff;
+`
+
+const CartItem = styled.View`
+   padding: 10px 30px;
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+`
+
+const CartItemLeft = styled.View`
+   flex-direction: row;
+   align-items: center;
+`
+
+const CartItemRight = styled.View`
+   flex-direction: row;
+   align-items: center;
+`
+
+const CartItemImage = styled.Image`
+   width: 50px;
+   height: 50px;
+   object-fit: cover;
+`
+
+const CartItemInfo = styled.View`
+   margin-left: 10px;
+   margin-right: 5px;
+`
+
+const CartItemName = styled.Text`
+   font-size: 14px;
+   font-weight: 400;
+`
+
+const CartItemLabel = styled.Text`
+   color: #9c9ea7;
+`
+
+const CartItemQuantity = styled.View``
+
+const CartItemPrice = styled.Text`
+   font-size: 13px;
+   color: #535665;
+`
+
+const CartBilling = styled.View`
+   padding: 10px 30px;
+   background: #fff;
+`
+
+const CartBillingHeading = styled.Text`
+   font-weight: 500;
+   font-size: 13px;
+   margin-bottom: 10px;
+   color: #282c3f;
+`
+
+const CartBillingDetail = styled.View`
+   flex-direction: row;
+   justify-content: space-between;
+   align-items: center;
+   margin-bottom: 8px;
+`
+
+const CartBillingDetailText = styled.Text`
+   font-size: 13px;
+   color: #686b78;
+`
+
+const Divider = styled.View`
+   background-color: ${props => props.color || '#e9e9eb'};
+   height: ${props => props.height || '1px'};
+   margin-bottom: ${props => props.margin || '0px'};
+`
+
+const CartFooter = styled.View`
+   flex-direction: row;
+   padding: 0px 30px;
+   height: 40px;
+   align-items: center;
+   justify-content: space-between;
+   background: #fff;
+`
+
+const CartFooterText = styled.Text`
+   font-weight: 600;
+   color: #282c3f;
+`
