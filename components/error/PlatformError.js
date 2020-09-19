0fda5e163ae94ceea31d4788626fcea3828820ef
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-09-19 16:12:07 +0530

    fix: remove visual setting from error page

--- components/error/PlatformError.js
+++ components/error/PlatformError.js
@@ -1,41 +1,23 @@
 import React from 'react'
 import { View } from 'react-native'
 import styled from 'styled-components/native'
-import { useAppContext } from '../../context/app'
 import { width } from '../../utils/Scalaing'
-import Header from '../Header'
 
-const PlatformError = ({ navigation }) => {
-   const { visual } = useAppContext()
-
+const PlatformError = () => {
    return (
-      <>
-         <Header navigation={navigation} />
-         <View
-            style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-         >
-            <Wrapper>
-               <Heading>Oops! It's not you. It's us.</Heading>
-               <CTAContainer>
-                  <CTA
-                     color={visual.color}
-                     onPress={() => window.location.reload()}
-                     highlight
-                  >
-                     <CTAText highlight color={visual.color}>
-                        Reload
-                     </CTAText>
-                  </CTA>
-                  <CTA
-                     color={visual.color}
-                     onPress={() => navigation.navigate('Home')}
-                  >
-                     <CTAText color={visual.color}>Back to Home</CTAText>
-                  </CTA>
-               </CTAContainer>
-            </Wrapper>
-         </View>
-      </>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+         <Wrapper>
+            <Heading>Oops! It's not you. It's us.</Heading>
+            <CTAContainer>
+               <CTA onPress={() => window.location.reload()} highlight>
+                  <CTAText highlight>Reload</CTAText>
+               </CTA>
+               <CTA onPress={() => window.location.href('/')}>
+                  <CTAText>Back to Home</CTAText>
+               </CTA>
+            </CTAContainer>
+         </Wrapper>
+      </View>
    )
 }
 
@@ -67,13 +49,12 @@ const CTAContainer = styled.View`
 
 const CTA = styled.TouchableOpacity`
    padding: 8px;
-   background-color: ${props =>
-      props.highlight ? props.color || '#E9ECEE' : '#fff'};
+   background-color: ${props => (props.highlight ? '#EE534F' : '#fff')};
    border: 1px solid ${props => props.color || '#E9ECEE'};
    border-radius: 2px;
    margin: 8px;
 `
 
 const CTAText = styled.Text`
-   color: ${props => (props.highlight ? '#fff' : props.color || '#666')};
+   color: ${props => (props.highlight ? '#fff' : '#EE534F')};
 `
