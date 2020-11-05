2ca4bae1a60b9c3f4ea0ae97922907e39ffb4d18
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-11-05 14:44:28 +0530

    style: nutrition component

--- components/Nutrition.js
+++ components/Nutrition.js
@@ -24,90 +24,90 @@ const Nutrition = ({ values }) => {
    return (
       <Wrapper>
          {/* <Heading>Nutrition</Heading> */}
-         <Calories>Calories {values.calories}</Calories>
+         <Calories>
+            <CaloriesText> Calories</CaloriesText>
+            <CaloriesText>{values.calories}</CaloriesText>
+         </Calories>
          <Table>
             <TableHead>
-               <TableHeadText>Amount/Serving</TableHeadText>
+               <TableHeadText></TableHeadText>
+               <TableHeadText>Amount</TableHeadText>
                <TableHeadText>% Daily Value</TableHeadText>
             </TableHead>
             <TableBody>
                <TableRow>
-                  <TableRowText>
-                     Total Fat{' '}
-                     <NutritionValue>{values.totalFat} g</NutritionValue>
-                  </TableRowText>
+                  <TableRowText title>Total Fat </TableRowText>
+                  <TableRowText>{values.totalFat} g</TableRowText>
                   <TableRowText>
                      {getDailyValue('Total Fat', values.totalFat)}
                   </TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText inset>
-                     Saturated Fat{' '}
-                     <NutritionValue>{values.saturatedFat} g</NutritionValue>
+                  <TableRowText inset small title>
+                     Saturated Fat
                   </TableRowText>
-                  <TableRowText>
+                  <TableRowText small>{values.saturatedFat} g</TableRowText>
+                  <TableRowText small>
                      {getDailyValue('Saturated Fat', values.saturatedFat)}
                   </TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText inset>
-                     Trans Fat{' '}
-                     <NutritionValue>{values.transFat} g</NutritionValue>
+                  <TableRowText inset small title>
+                     Trans Fat
                   </TableRowText>
+                  <TableRowText small>{values.transFat} g</TableRowText>
+                  <TableRowText small></TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText>
-                     Cholesterol{' '}
-                     <NutritionValue>{values.cholesterol} mg</NutritionValue>
-                  </TableRowText>
+                  <TableRowText title>Cholesterol</TableRowText>
+                  <TableRowText>{values.cholesterol} mg</TableRowText>
                   <TableRowText>
                      {getDailyValue('Cholesterol', values.cholesterol)}
                   </TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText>
-                     Sodium <NutritionValue>{values.sodium} mg</NutritionValue>
-                  </TableRowText>
+                  <TableRowText title>Sodium</TableRowText>
+                  <TableRowText>{values.sodium} mg</TableRowText>
                   <TableRowText>
                      {getDailyValue('Sodium', values.sodium)}
                   </TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText>
-                     Total Carbs{' '}
-                     <NutritionValue>
-                        {values.totalCarbohydrates} g
-                     </NutritionValue>
-                  </TableRowText>
+                  <TableRowText title>Total Carbs</TableRowText>
+                  <TableRowText>{values.totalCarbohydrates} g</TableRowText>
                   <TableRowText>
                      {getDailyValue('Total Carbs', values.totalCarbohydrates)}
                   </TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText inset>
-                     Dietary Fibre{' '}
-                     <NutritionValue>{values.dietaryFibre} g</NutritionValue>
+                  <TableRowText inset small title>
+                     Dietary Fibre
                   </TableRowText>
-                  <TableRowText>
+                  <TableRowText small>{values.dietaryFibre} g</TableRowText>
+                  <TableRowText small>
                      {getDailyValue('Dietary Fibre', values.dietaryFibre)}
                   </TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText inset>
-                     Sugars <NutritionValue>{values.sugars} g</NutritionValue>
+                  <TableRowText inset small title>
+                     Sugars
                   </TableRowText>
+                  <TableRowText small>{values.sugars} g</TableRowText>
+                  <TableRowText small></TableRowText>
                </TableRow>
                <TableRow>
-                  <TableRowText>
-                     Protein <NutritionValue>{values.protein} g</NutritionValue>
-                  </TableRowText>
+                  <TableRowText title>Protein</TableRowText>
+                  <TableRowText>{values.protein} g</TableRowText>
+                  <TableRowText></TableRowText>
                </TableRow>
             </TableBody>
             <TableFooter>
                <TableFooterText>Vitamin A {values.vitaminA}%</TableFooterText>
-               <TableFooterText>Vitamin C {values.vitaminC}%</TableFooterText>
+               <TableFooterText right>
+                  Vitamin C {values.vitaminC}%
+               </TableFooterText>
                <TableFooterText>Calcium {values.calcium}%</TableFooterText>
-               <TableFooterText>Iron {values.iron}%</TableFooterText>
+               <TableFooterText right>Iron {values.iron}%</TableFooterText>
             </TableFooter>
          </Table>
       </Wrapper>
@@ -126,60 +126,73 @@ const Heading = styled.Text`
    margin-bottom: 0.5rem;
 `
 
-const Calories = styled.Text`
-   font-weight: 600;
+const Text = styled.Text`
    color: #555b6e;
-   margin-bottom: 0.25rem;
+   font-size: 14px;
+   font-weight: normal;
+`
+
+const Calories = styled.View`
+   flex-direction: row;
+   align-items: center;
+   justify-content: space-between;
+   padding: 8px 0px;
+   border-color: #555b6e;
+   border-bottom-width: 1px;
+`
+
+const CaloriesText = styled(Text)`
+   font-weight: 600;
+   font-size: 20px;
 `
 
 const Table = styled.View``
 
 const TableHead = styled.View`
-   flex-direction: row;
-   align-items: center;
-   justify-content: space-between;
-   border-bottom: 2px solid #555b6e;
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr;
 `
 
-const TableHeadText = styled.Text`
+const TableHeadText = styled(Text)`
    font-size: 14px;
-   color: #555b6e;
+   font-weight: 600;
+   text-align: right;
+   line-height: 2.2rem;
 `
 
 const TableBody = styled.View``
 
 const TableRow = styled.View`
-   flex-direction: row;
-   align-items: center;
-   justify-content: space-between;
-   height: 1.5rem;
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr;
+   height: 2rem;
 `
 
-const TableRowText = styled.Text`
+const TableRowText = styled(Text)`
    font-weight: 600;
-   color: #555b6e;
+   font-size: 16px;
+   min-width: 60px;
+   text-align: ${props => (props.title ? 'left' : 'right')};
    ${props =>
       props.inset &&
       css`
          padding-left: 1rem;
-         font-weight: normal;
+      `}
+   ${props =>
+      props.small &&
+      css`
+         font-size: 14px;
       `}
 `
 
-const NutritionValue = styled.Text`
-   font-weight: normal;
-`
-
 const TableFooter = styled.View`
-   flex-direction: row;
-   align-items: center;
-   justify-content: space-between;
-   min-height: 1.5rem;
-   flex-wrap: wrap;
+   display: grid;
+   grid-template-columns: 1fr 1fr;
 `
 
-const TableFooterText = styled.Text`
-   color: #555b6e;
+const TableFooterText = styled(Text)`
+   font-size: 16px;
    font-weight: 600;
-   margin-right: 8px;
+   line-height: 2rem;
+   text-align: ${props => (props.right ? 'right' : 'left')};
 `
