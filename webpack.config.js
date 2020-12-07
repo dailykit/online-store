f1233299377075e7dc83cb2f4af4b54fdb3759dc
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-12-07 15:11:01 +0530

    chore: update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,11 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config')
 
 module.exports = async function (env, argv) {
-   const config = await createExpoWebpackConfigAsync(
-      {
-         ...env,
-         babel: {
-            dangerouslyAddModulePathsToTranspile: ['@ui-kitten/components'],
-         },
-         offline: false,
-      },
-      argv
-   )
-   config.output = {
-      filename: '[name].[contenthash].js',
+   const config = await createExpoWebpackConfigAsync(env, argv)
+   config.resolve.alias = {
+      'react-native-maps': 'react-native-web-maps',
+      'react-native': 'react-native-web',
+      'react-native-banner-carousel': 'react-native-web-banner-carousel',
    }
-   config.resolve.alias['react-native-maps'] = 'react-native-web-maps'
-   config.resolve.alias['react-native'] = 'react-native-web'
-   config.resolve.alias['react-native-banner-carousel'] =
-      'react-native-web-banner-carousel'
    return config
 }
