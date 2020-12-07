76c4f0d47893374604775a39919ff02104fc1aa0
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-12-07 18:50:55 +0530

    chore: updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const createExpoWebpackConfigAsync = require('@expo/webpack-config')
 
 module.exports = async function (env, argv) {
@@ -6,6 +7,10 @@ module.exports = async function (env, argv) {
       'react-native-maps': 'react-native-web-maps',
       'react-native': 'react-native-web',
       'react-native-banner-carousel': 'react-native-web-banner-carousel',
+   }
+   config.output = {
+      filename: '[name].[contenthash:6].bundle.js',
+      path: path.resolve(__dirname, 'web-build/'),
    }
    return config
 }
