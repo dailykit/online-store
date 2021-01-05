d8148abbb9eabf19a0216301045e835c5500daf7
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2021-01-05 15:55:58 +0530

    fix: always show custom login

--- utils/index.js
+++ utils/index.js
@@ -166,6 +166,7 @@ export const isKeycloakSupported = () => {
    console.log({ browser, version })
    const v = +version.split('.')[0]
    const { userAgent } = window.navigator
+   return false
    if (userAgent.includes('Mac') || userAgent.includes('Apple')) {
       return false
    }
